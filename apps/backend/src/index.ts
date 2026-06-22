1adc052a0511904a841ed2b5c21f7f21a734119c
Tanveer-rajpurohit <tanveersinghrajpurohit4@gmail.com>
2026-06-22 17:40:16 +0530

    setup backend

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("CausalFunnel Backend API is running!");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
